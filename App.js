ea87c02ca4fa48f7539a138678591e8ce762a234
Timothy Stokes <timstokes746329@gmail.com>
2017-10-03 20:52:00 +0100

    Initial commit- simple express.js setup

---
+++ App.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Define the port to run on
+app.set('port', 3000);
+
+app.use(express.static(path.join(__dirname, 'Public')));
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
